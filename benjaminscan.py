8e6c4b1d994057c8dc9b933b67a313fbd5548cb9
?0 <86694445+BenjaminXN@users.noreply.github.com>
2021-11-27 09:02:43 +0700

    Add files via upload

---
+++ benjaminscan.py
@@ -0,0 +1,31 @@
+import socket
+import argparse
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+def main():
+   p = argparse.ArgumentParser(description="single port scan")
+   p.add_argument("--host",
+                  type=str,
+                  dest="host",
+                  help="masukan target")
+
+   p.add_argument("--port",
+                  type=int,
+                  dest="port",
+                  help="masukan port")
+   inp = p.parse_args()
+
+   try:
+       s.connect((inp.host,inp.port))
+       print(f"[+] {inp.host}:{inp.port} open")
+   except:
+       print(f"[-] {inp.host}:{inp.port} close")
+
+   
+
+if __name__ == "__main__":
+    main()
+
+
+
